aa7e6746580db08ec87f77ffbf86acf4c55b6a63
tharpegeo <28299627+tharpegeo@users.noreply.github.com>
2021-06-16 18:33:51 +0530

    centered mobile nav + added padding

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //defining const - variable cannot be redifined or updated; query selector selects the id of btnHamburger
 const btnHamburger = document.querySelector('#btnHamburger');
 const headerMobile = document.querySelector('.header__mobile')
-
 //addEventListener - on click it runs the funtion. if btnHamburger element's classlist has open class, then it will remove, else it will add. So this triggers the on off loop for mobile menu.
 
 btnHamburger.addEventListener('click', function() {
@@ -18,6 +17,7 @@ btnHamburger.addEventListener('click', function() {
     }
     
 });
+
 headerMobile.addEventListener('click', function() {
     console.log('link clicked');
 
