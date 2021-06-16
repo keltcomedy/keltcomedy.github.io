16c53dc1c0c815e832a23471d5b5e496ea828f74
tharpegeo <28299627+tharpegeo@users.noreply.github.com>
2021-06-16 17:49:08 +0530

    js clean up

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //defining const - variable cannot be redifined or updated; query selector selects the id of btnHamburger
 const btnHamburger = document.querySelector('#btnHamburger');
 const headerMobile = document.querySelector('.header__mobile')
-const section = document.getElementsByTagName('section')
+
 //addEventListener - on click it runs the funtion. if btnHamburger element's classlist has open class, then it will remove, else it will add. So this triggers the on off loop for mobile menu.
 
 btnHamburger.addEventListener('click', function() {
@@ -29,28 +29,6 @@ headerMobile.addEventListener('click', function() {
     
 });
 
-section.addEventListener('click', function() {
-    console.log('link clicked');
-
-    if(btnHamburger.classList.contains('open')) {
-        btnHamburger.classList.remove('open');       
-        headerMobile.classList.remove('open');       
-    }
-
-    
-});
 
 
 
-// headerMenu.addEventListener('click', function() {
-//     console.log('click hamburger');
-
-//     if(btnHamburger.classList.contains('open')) {
-//         btnHamburger.classList.remove('open')        
-//     }
-
-//     else {
-//         btnHamburger.classList.add('open')
-//     }
-    
-// });
