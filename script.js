27336da34946057dac8c2932078c4cf923ff4e2c
tharpegeo <28299627+tharpegeo@users.noreply.github.com>
2021-06-16 17:43:26 +0530

    mobile nav menu completed

--- script.js
+++ script.js
@@ -1,8 +1,56 @@
-$(document).on("click","a",function(e){
-    e.preventDefault();
-    var id = $(this).attr("href"),
-        topSpace = 30;
-    $('html, body').animate({
-      scrollTop: $(id).offset().top - topSpace
-    }, 800);
+//defining const - variable cannot be redifined or updated; query selector selects the id of btnHamburger
+const btnHamburger = document.querySelector('#btnHamburger');
+const headerMobile = document.querySelector('.header__mobile')
+const section = document.getElementsByTagName('section')
+//addEventListener - on click it runs the funtion. if btnHamburger element's classlist has open class, then it will remove, else it will add. So this triggers the on off loop for mobile menu.
+
+btnHamburger.addEventListener('click', function() {
+    console.log('click hamburger');
+
+    if(btnHamburger.classList.contains('open')) {
+        btnHamburger.classList.remove('open');       
+        headerMobile.classList.remove('open');       
+    }
+
+    else {
+        btnHamburger.classList.add('open')
+        headerMobile.classList.add('open');
+    }
+    
 });
+headerMobile.addEventListener('click', function() {
+    console.log('link clicked');
+
+    if(btnHamburger.classList.contains('open')) {
+        btnHamburger.classList.remove('open');       
+        headerMobile.classList.remove('open');       
+    }
+
+    
+});
+
+section.addEventListener('click', function() {
+    console.log('link clicked');
+
+    if(btnHamburger.classList.contains('open')) {
+        btnHamburger.classList.remove('open');       
+        headerMobile.classList.remove('open');       
+    }
+
+    
+});
+
+
+
+// headerMenu.addEventListener('click', function() {
+//     console.log('click hamburger');
+
+//     if(btnHamburger.classList.contains('open')) {
+//         btnHamburger.classList.remove('open')        
+//     }
+
+//     else {
+//         btnHamburger.classList.add('open')
+//     }
+    
+// });
